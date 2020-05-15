302d55aa1ddf5c2dd3e3c6afe0a7a185f56ca188
Jonathan Vuillemin <ekkinox@gmail.com>
2020-05-15 15:41:20 +0200

    Added logs

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     OAT\Bundle\Lti1p3Bundle\Lti1p3Bundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
