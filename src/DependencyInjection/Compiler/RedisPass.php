841ce85450c94b84cda0517f1a36e2e3288ab8df
Péter Halász <halaszpeterzsolt@gmail.com>
2022-07-13 12:55:23 +0200

    feat: added redis namespace support

---
+++ src/DependencyInjection/Compiler/RedisPass.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace App\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RedisPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        $redisCacheNamespace = $container->getParameter('cache.redis.namespace');
+        $container
+            ->getDefinition('cache.adapter.redis')
+            ->setArgument('$namespace', $redisCacheNamespace);
+    }
+}
