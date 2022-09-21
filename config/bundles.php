01202af08edc8cd30d43dd9ef493529619799d2a
ar0aot <arnas.kasparas@taotesting.com>
2022-09-21 11:53:38 +0300

    feature: enabled cors call.

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     OAT\Bundle\HealthCheckBundle\HealthCheckBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
