f5032e2324456a93f1206ff0f06619ae9eae77e1
Vitalii Shtykhno <kilatib@gmail.com>
2026-08-27 09:54:49 +0200

    chore: fix ACS action SPDX copyright years

--- src/Action/Tool/Ajax/AcsServiceClientAction.php
+++ src/Action/Tool/Ajax/AcsServiceClientAction.php
@@ -1,7 +1,7 @@
 <?php
 
-// SPDX-FileCopyrightText: 2012-2026 Open Assessment Technologies S.A.
-// Copyright (C) 2024 (original work) Open Assessment Technologies SA ;
+// SPDX-FileCopyrightText: 2020-2026 Open Assessment Technologies S.A.
+// Copyright (C) 2020-2026 (original work) Open Assessment Technologies S.A.
 //
 // SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-TAO-Commercial-License
 
